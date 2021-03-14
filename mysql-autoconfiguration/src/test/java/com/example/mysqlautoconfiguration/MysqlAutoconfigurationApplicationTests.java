07e6bb1122509f9278dad93a3f01a41e852937fb
Win Zaw Oo <iwinzawcool@gmail.com>
2021-03-14 12:53:42 +0630

    first commit

---
+++ mysql-autoconfiguration/src/test/java/com/example/mysqlautoconfiguration/MysqlAutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlautoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlAutoconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
